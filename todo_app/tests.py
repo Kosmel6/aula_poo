37b1f1d0edf35f93e01761f4d863b69e5a46d5b1
Thomas Lincoln <thmas5952@gmail.com>
2022-10-22 12:34:59 -0300

    Todo app

---
+++ todo_app/tests.py
@@ -0,0 +1,31 @@
+from todoitem import TodoItem
+from user import User
+from todolist import TodoList
+
+
+def test_categoria():
+    item1 = TodoItem("Testar o código", 0);
+    assert item1.nivel == 0;
+
+def test_modify():
+    item1 = TodoItem("Testar o código", 0);
+    item1.modify(2)
+    assert item1.nivel == 2;
+
+def test_ordem():
+    dono = User('Thomas', 'tho@mas.com')
+    lista = TodoList(dono);
+    # 0 = baixo, 1 = médio e 2 = alto
+    item = TodoItem("fazer bolo", 0);
+    item2 = TodoItem("comer alface", 2);
+    item3 = TodoItem("Jogar xadrez", 1);
+    lista.add(item);
+    lista.add(item2);
+    lista.add(item3);
+    item.modify(1);
+    lista.add(item);
+
+
+    assert lista.verify() == 1;
+
+
---
+++ todo_app/todolist.py
@@ -0,0 +1,49 @@
+from user import User
+from todoitem import TodoItem
+
+class TodoList:
+    def __init__(self, owner: User):
+        self.owner = owner
+        self.list = []
+
+    def add(self, item: TodoItem):
+        tamanho = len(self.list)
+        i = 0;
+        for i in range(tamanho):
+            if self.list[i] == item:
+                self.list.remove(item);
+                i = tamanho;
+        i = 0;
+        for i in range(tamanho):
+            if self.list[i+1].nivel > item.nivel:
+                self.list.insert(i, item);
+                i = tamanho;
+
+    def get(self, index):
+        return self.list[index]
+
+    def get_owner(self):
+        return self.owner
+
+    def complete_item(self, index):
+        self.list[index].complete()
+
+    def remove(self, index):
+        self.list.pop(index)
+
+    def size(self):
+        return len(self.list)
+
+    def find(self, description):
+        for item in self.list:
+            if item.description == description:
+                return item
+
+    def verify(self):
+
+        for item_array in self.list:
+            if (item_array.nivel < valor):
+                return 0;
+            valor = item_array.nivel
+        return 1
+
